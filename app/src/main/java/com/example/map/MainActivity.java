e3f38e3ed51eb77aa63d848cf80d01cc427507a5
anhnguyenphung <anhnguyenphung@email.arizona.edu>
2020-01-18 02:44:52 -0700

    First Commit

---
+++ app/src/main/java/com/example/map/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.map;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMessage();
+            }
+        });
+    }
+
+    public void sendMessage() {
+        Intent myIntent = new Intent(MainActivity.this, MapsActivity.class);
+        MainActivity.this.startActivity(myIntent);
+    }
+}
