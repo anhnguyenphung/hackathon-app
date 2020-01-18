526d46e18c9e044e73a276d123eee3ba7aa26884
Pegasust <35638399+Pegasust@users.noreply.github.com>
2020-01-18 14:39:08 -0700

    Update MapsActivity.java

--- app/src/main/java/com/example/map/MapsActivity.java
+++ app/src/main/java/com/example/map/MapsActivity.java
@@ -3,6 +3,7 @@ package com.example.map;
 import android.location.Location;
 import android.location.LocationListener;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -21,12 +22,36 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener,
         LocationListener {
-
+            
+    // Only update location marker per 500 meters
+    public static final float UPDATE_DISTANCE = 500.0;
+    public static final int ZOOM_LEVEL = 17;
+            
     private GoogleMap mMap;
-
+    private GoogleApiClient gApiClient;
+    private Marker lastMarker;
+    private LocationRequest locationRequest;
+    private Location lastLoc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Ensure that location access is permitted by user.
+        if(Utils.checkLocationPerm(this))
+        {
+            Log.d("MapsActivity.onCreate", "Location perm already granted.");
+        }
+        else
+        {
+            Log.d("MapsActivity.onCreate", "Location perm is prompted.");
+        }
+        // Ensure that google play services is installed.
+        // Since this project is heavily dependent on it, if GP Services is
+        // not installed, just exit out.
+        if(!Utils.checkGooglePlayServices(this))
+        {
+            Log.e("MapsActivity.onCreate", "Google Play Services not available!");
+            finish();
+        }
         setContentView(R.layout.activity_maps);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -36,16 +61,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
+        gApiClient = Utils.initializeMaps(this);
         // Add a marker in Sydney, Australia, and move the camera.
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//         LatLng sydney = new LatLng(-34, 151);
+//         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
     @Override
     public void onLocationChanged(Location location) {
-
+        Log.d("MapsActivity.onLocationChanged", "called");
+        // Update loction marker when reached threshold
+        if(lastLoc == null || lastLoc.distanceTo(location) >= UPDATE_DISTANCE)
+        {
+            // Remove last marker.
+            if(lastMarker != null)
+            {
+                lastMarker.remove();
+            }
+            double[] latlng = Utils.getLatLng(location);
+            LatLng currLatLng = new LatLng(latlng[0], latlng[1]);
+            MarkerOptions markerOptions = new MarkerOptions()
+                .position(currLatLng)
+                .title("Current Location");
+            lastMarker = mMap.addMarker(markerOptions);
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(currLatLng));
+            mMap.animateCamera(CameraUpdateFactory.zoomTo(ZOOM_LEVEL));
+        }
     }
 
     @Override
@@ -65,7 +107,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
-
+        // When device is connected, attempt to do location updates.
+        Utils.pollLocationUpdate(a, 1000, 1000);
     }
 
     @Override
